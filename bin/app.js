db6a5a6dc5fbc3c49423f9162d6b0d7dcf5d6138
Kyle Sandhage <ksandhage@relaynetwork.com>
2017-09-27 16:47:18 -0400

    Added second question.

--- bin/app.js
+++ bin/app.js
@@ -2,7 +2,4 @@ const takeSurvey = require('../survey/take_survey');
 const debug = require('debug')('app');
 const pino = require('pino')();
 
-pino.info('Application started...');
-takeSurvey.execute().then(function () {
-    pino.info('... Application ended.');
-});
+takeSurvey.execute();
--- survey/take_survey.js
+++ survey/take_survey.js
@@ -8,19 +8,43 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
+let record = new Object();
 
 exports.execute = function () {
-    pino.debug('survey running...');
+    pino.info('survey running...');
+    askDadName().then(response => {
+        console.log(`${response} ... OF COURSE YES THIS IS IT YES`);
+        
+        askFeelings().then(resp => {
+            console.log(`${resp} ... SO THAT IS FEELING. YES.`);
+
+            console.log('HERE IS WHAT I KNOW ABOUT DAD: %o', record.dads_name);
+            console.log('HERE IS WHAT FEELING IS: %o', record.feeling);
+            pino.info('... survey ended.');
+        })
+    });
+};
+
+function askDadName() {
     return new Promise(function (resolve, reject) {
-        rl.question('TELL ME THE DAD\'S NAME SO THAT I MAY BECOME DJYNN:', (answer) => {
-            console.log(`${answer} ... OF COURSE YES THIS IS IT YES`);
-            
-            pino.debug('... survey ended.');
+        rl.question('TELL ME THE DAD\'S NAME SO THAT I MAY BECOME DJYNN: \n', (answer) => {
+            record.dads_name = answer;
+
             rl.close();
 
             resolve(answer);
         });
     });
+}
 
- 
-};
+function askFeelings() {
+    return new Promise(function (resolve, reject) {
+        rl.question('TELL ME NOW DO YOU FEEL: \n', (answer) => {
+            record.feeling = answer;
+
+            rl.close();
+
+            resolve(answer);
+        });
+    });
+}
